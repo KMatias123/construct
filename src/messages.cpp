617af0cb6e71ce74e5d31fd035e47e1ad5ade772
matias <KMatias1@protonmail.com>
2024-02-12 16:46:56 +0200

    Removed everything color and formatting related in messages.cpp

--- src/messages.cpp
+++ src/messages.cpp
@@ -6,16 +6,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include <format>
 
-#define COLOR_RED "\033[0;31m"
-#define COLOR_RESET "\033[0;0m"
-
-// TODO: pass down filename in deconstruct.cpp and print it here.
-// TODO: add startup arg/check if color_prompt = yes
 void invalidOperand(std::string filename, std::vector<std::string> line_split, size_t line) {
-
-
   size_t length = line_split[0].length() + line_split[1].length() + 1;
   std::string* filler = new std::string;
   
@@ -23,11 +15,8 @@ void invalidOperand(std::string filename, std::vector<std::string> line_split, s
     filler->append(" ");
   }
 
-  std::string formatted = std::format("Error: Invalid operand at line {} in file {}\n\n{} {} {} {}\n{} ^\n",
-      line, filename.c_str(), line_split[0].c_str(), line_split[1].c_str(),
-      line_split[2].c_str(), line_split[3].c_str(), filler->c_str());
-  
-  std::cerr << formatted;
+  std::cerr << "Error: Invalid operand at line " << line << " in file " << filename.c_str() << "\n\n" << line_split[0].c_str() << " " << line_split[1].c_str() <<
+    " " << line_split[2].c_str() << " " << line_split[3].c_str() << "\n" << filler->c_str() << " ^\n";
 
   exit(EXIT_FAILURE);
 }
